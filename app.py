87cc56b1ae9dfb603fe967b932ac35c0f6773b6f
Haoyuan Dong <hlj12530@gmail.com>
2025-01-16 21:28:18 -0500

    update

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import dash
 from dash import html, dcc
-import dash_leaflet as dl
 from dash.dependencies import Input, Output
 from src.pages import home_layout, home_register_callbacks, company_info_input_layout, company_info_input_register_callbacks, success_layout
 
